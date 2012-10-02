28c6ccfec874b6970dd3d97ad5e3e8018fa3f2ec
Andy Miller <stiggybr0@gmail.com>
2012-10-01 23:48:34 -0700

    separated run_classify from menu and changed argument functionality

--- DefultPackage/main.py
+++ DefultPackage/main.py
@@ -13,23 +13,13 @@ Created on Sep 28, 2012
 '''
 
 def main():
-    if(len(sys.argv) != 1):
+    if(len(sys.argv) != 3):
         print "Incorrect number of arguments."
-        print "Usage: python main.py" 
+        print "Usage: python main.py tn-train.txt tn-test.txt" 
         sys.exit()
     menu()
     
-
-def file_length(fi):
-    with open(fi) as f:
-        for i, l in enumerate(f):
-            pass
-    return i + 1
-
-
 def p_train(num_epochs, training_data):
-   
-    num_examples = file_length(training_data)
     y = []
     x1 = []
     x2 = []
@@ -37,8 +27,7 @@ def p_train(num_epochs, training_data):
     u = []
     train_data = open(training_data, "rt")
     
-    for line in train_data:
-        i = 0
+    for i, line in enumerate(train_data):
         a, b, c = [float(x) for x in line.split()]
         y.append(a)
         x1.append(b)
@@ -48,12 +37,9 @@ def p_train(num_epochs, training_data):
             temp_x1 = y[i] * x1[i]
             temp_x2 = y[i] * x2[i]
             w[0] = w[0] + temp_x1
-            w[1] = w[1] + temp_x2
-        i += 1     
+            w[1] = w[1] + temp_x2  
     return w
                         
-      
-        
 def p_classify(x, w):
     if(len(w) != 2):
         print "cannot classify x with this weight vector: wrong size"
@@ -62,42 +48,46 @@ def p_classify(x, w):
         print "cannot classify with this test example vector: wrong size"
         sys.exit()
     if(((w[0] * x[0]) + (w[1] *x[1])) > 0):
-        print "1"
         return 1
     else:
-        print "-1"
         return -1
 
 
+def run_classify(w, testing_data):
+    test_data = open(testing_data, "rt")
+    y_data = []
+    y_classified = []
+    x1 = []
+    x2 = []
+    for i, line in enumerate(test_data):
+        a, b, c = [float(x) for x in line.split()]
+        y_data.append(a)
+        x1.append(b)
+        x2.append(c)   
+        x = [x1[i], x2[i]]
+        y_classified.append(p_classify(x,w))
+    print "here is how we classified the test data: "
+    print y_classified
+
 
 def menu():
+    training_data = sys.argv[1]
+    testing_data = sys.argv[2]
     count = 0
     while count == 0:
-        print '''What would you like to do?
+        print """
+What would you like to do?
 0) Exit
-1) Train & Classify'''
+1) Train & Classify"""
         action = raw_input("> ")
         if action == str(1):
             print "-------------------------------------------------"
-            print "What file would you like to train with?"
-            training_data = raw_input("> ")
             print "How many epochs would you like to train with?"
             num_epochs = raw_input("> ")
-            print "What file would you like to test with?"
-            testing_data = raw_input("> ")
+            print "-------------------------------------------------"
             print "Training with " + num_epochs + " epochs of " + training_data + " then classifying data in " + testing_data
             w = p_train(num_epochs, training_data)
-            test_data = open(testing_data, "rt")
-            y = []
-            x1 = []
-            x2 = []
-            for line in test_data:
-                a, b, c = [float(x) for x in line.split()]
-                y.append(a)
-                x1.append(b)
-                x2.append(c)   
-            x = [x1[0], x2[0]]
-            p_classify(x,w)
+            run_classify(w,testing_data)
             print "-------------------------------------------------"
         elif action == str(0):
             sys.exit()
