dc1a8f3ec6fc08c4deece757ecf9fe54115b0c2d
James Cunningham <james@jtcunning.com>
2012-10-31 23:03:09 -0700

    Init_graph

--- Assignment3/main.py
+++ Assignment3/main.py
@@ -3,7 +3,7 @@ import csv
 import math
 
 '''
-Created on Oct 16, 2012
+Created on Oct 30, 2012
 
 @author: James Cunningham & Andy Miller
 '''
@@ -19,155 +19,25 @@ Output: N/A
 def main():
     if(len(sys.argv) != 3):
         print "Incorrect number of arguments."
-        print "Usage: python main.py monks-1-train.csv monks-1-test.csv" 
+        print "Usage: python main.py cluster-data.csv kmeans-num" 
         sys.exit()
     menu()
     
-    
 '''
-Function: value_init
-Purpose: static training algorithm
-Input: CSV of monk data.
-Output: 2d array with values and probabilities.
-'''
-    
-def value_init(csvname):
-    datamap = []
-    classline = []
-    line1 = []
-    line2 = []
-    line3 = []
-    line4 = []
-    line5 = []
-    line6 = []
-    
-    csvmap = csv.reader(open(csvname))
-    
-    for row in csvmap:
-        classify, x1, x2, x3, x4, x5, x6 = [int(x) for x in row] 
-        classline.append(classify)
-        line1.append(x1)
-        line2.append(x2)
-        line3.append(x3)
-        line4.append(x4)
-        line5.append(x5)
-        line6.append(x6)
-
-    datamap.append(classline)
-    datamap.append(line1)
-    datamap.append(line2)
-    datamap.append(line3)
-    datamap.append(line4)
-    datamap.append(line5)
-    datamap.append(line6)
-
-    return datamap
-
-'''
-Function: entropy_calc
-Purpose: Calculate entropies of each data set
-Input: datamap of initialized CSVs
-Output: appended entropies
+Function:init_graph
+Purpose: adds raw input into list to build graph.
+Input: raw sys.arg
+Output: Array of points
 '''
 
-def entropy_calc(datamap):
-    counter = 0
-    entropylist = []
-    entropyval = 0.0
-    pos = []
-    neg = []
-    monkset = []
-    monkamount = 0
-    prob = []
-    countmore = 0
-    totalentropy = -.5* math.log(.5, 2) - .5* math.log(.5, 2)    
-    infolist = [0] * 6
-    entropymap  = []
-    probstats = []
-    itemcount = []
-    itemmap = []
+def init_graph(cluster_data):
+    graph_data=[]
+    cluster_file = csv.reader(open(cluster_data, "rt"))
+    for line in cluster_file:
+        graph_data.append(line)
+    print graph_data  
+    return graph_data  
     
-    for  list in datamap:
-        if (counter == 0):
-            monkset = list
-            for item in monkset:
-                if(item == 1):
-                    monkamount = monkamount + 1
-        else:
-            if(counter == 1 or counter == 2 or counter == 4):
-                pos = [0] * 3
-                prob = [0] * 3
-                neg = [0] * 3
-                itemcount = [0] * 3
-            elif(counter == 3 or counter == 6):
-                pos = [0] * 2
-                prob = [0] * 2
-                neg = [0] * 2
-                itemcount = [0] * 2
-            elif(counter == 5):
-                pos = [0] * 4
-                prob = [0] * 4
-                neg = [0] * 4
-                itemcount = [0] * 4
-            else:
-                print "Sanity Check: Failed!"
-            
-            for i, item in enumerate(list):
-                if (monkset[i] == 1):
-                    pos[item-1] = pos[item-1] + 1
-                    
-                else:
-                    neg[item-1] = neg[item-1] + 1
-                
-                #itemcount[item-1] = itemcount[item-1] + 1
-            
-            for i in range(len(pos)):
-                itemcount[i] = pos[i] + neg[i]
-            
-            itemmap.append(itemcount)
-            
-            for i in range(len(prob)):
-                probpair= [0] * 2
-                probpair[0] = float(pos[i])/(float(pos[i])+float(neg[i]))
-                probpair[1] = float(neg[i])/(float(pos[i])+float(neg[i]))
-                countmore = countmore + 1
-                prob[i] = probpair
-           
-            countmore = 0
-            probstats.append(prob)
-            for item in prob:
-                if (item[0] != 1.0 and item[0] != 0.0):
-                    entropyval = 0.0 - item[0]*(math.log(item[0], 2)) - item[1]*(math.log(item[1], 2))
-                
-                else:
-                    entropyval = 0.0
-                
-                entropylist.append(entropyval)
-                entropyval = 0.0
-            entropymap.append(entropylist)
-            entropylist = []
-        counter = counter + 1
-        
-        for i, row in enumerate(entropymap):
-            infolist[i] = totalentropy
-            itemrow = itemmap[i]
-            entropyrow = entropymap[i]
-            for j, item in enumerate(row):
-                infolist[i] = infolist[i] - itemrow[j]/124.0*entropyrow[j]
-            
-    print infolist
-    return prob
-'''
-Function: stump_creator
-Purpose: take entropies and create single featured decision stump
-Input: entropylist
-Output: decisionstump
-'''
-
-def info_gain(datamap, entropylist):
-    
-    
-    return
     
 '''
 Function: menu
@@ -177,20 +47,19 @@ Output: write to files.
 '''
 
 def menu():
-    training_data = sys.argv[1]
-    testing_data = sys.argv[2]
+    cluster_data = sys.argv[1]
+    kmeans = sys.argv[2]
     count = 0
     while count == 0:
         print """
-What would you like to do?
-0) Exit
-1) See work so far"""
+        What would you like to do?
+        0) Exit
+        1) See work so far"""
         action = raw_input("> ")
         if action == str(1):
             print "-------------------------------------------------"
-            dmap = value_init(training_data) 
-            entropylist = entropy_calc(dmap)
-            gainlist = info_gain(dmap, entropylist)
+            print "Initializing graph."
+            graph_data = init_graph(cluster_data)
                                                                                
             print "-------------------------------------------------"
               
